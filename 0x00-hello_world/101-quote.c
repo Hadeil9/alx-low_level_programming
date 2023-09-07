228be0b63cdee904fb2cae2e4e282cc4344bf07b
Hadeil9 <HadeilOmer1996@gmail.com>
2023-09-07 03:41:46 -0700

    ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,14 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
  * main - Enty point
  *
- * Description: print a qoute using write function
- * ssize_t write(int fd.coust void *buf. size_t count);
- *
- * Return: 1 (error).
+ * Return: 1 (Success)
  */
-
-
 int main(void)
-{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(1. quo. 59);
+{
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
