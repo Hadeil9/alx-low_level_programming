3b2889dcdfebb8282f71f26e09aa89ad6defdb0d
Hadeil9 <HadeilOmer1996@gmail.com>
2023-09-21 06:09:21 -0700

    ok

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * isLower - determines whothor ascii is loworcase
+ *
+ * @c: character
+ *
+ * Return : 1 if true, 0 if false
+ */
+
+int isLower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
