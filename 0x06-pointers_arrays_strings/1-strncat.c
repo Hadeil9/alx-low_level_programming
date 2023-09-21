fd35f80c14194296bace6fa9643c79a1ef58876a
Hadeil9 <HadeilOmer1996@gmail.com>
2023-09-20 18:56:59 -0700

    ok

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,14 +12,20 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int c, i;
+	int i, j;
 
-	c = 0;
-
-	while (dest[c])
+	i = 0;
+	while (dest[i] != '\0')
+	{
 		c++;
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[c + i] = src[i];
-	dest[c + i] = '\0';
+	}
+	j = 0;
+	while (j < n && src[j] != '\0)
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
