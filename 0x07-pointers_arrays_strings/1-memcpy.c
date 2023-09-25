418afb3ceda9f061f113e90b2044d9509a467ed9
Hadeil9 <HadeilOmer1996@gmail.com>
2023-09-25 16:59:57 -0700

    ok

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,11 +2,11 @@
 
 /**
  * *_memcpy - copies memory
- * @dest: memory size
- * @src: source
- * @n: length
+ * @dest: destination memory area
+ * @src: source memory area
+ * @n: number of bytes to copy
  *
- * Return: the point to dest
+ * Return: a pointer to the destination memory area
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
