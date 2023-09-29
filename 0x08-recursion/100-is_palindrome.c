db5ee67eba6866b4d8bef87672a2d4d888c78f1b
Hadeil9 <HadeilOmer1996@gmail.com>
2023-09-28 17:24:25 -0700

    done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,42 +4,22 @@
  * is_palindrome - Checks if a string is a palindrome.
  * @s: The string to check.
  *
- * Description: This function checks if the input string is a palindrome.
- * A palindrome is defined as a string that reads the same forward and backward.
- * The function uses a recursive approach to check if the first and last characters
- * of the string match. If they match, it recursively checks the remaining substring
- * until it reaches the middle of the string. If all characters match, the string is
- * a palindrome and the function returns 1. If any characters do not match, the string
- * is not a palindrome and the function returns 0.
- *
- * Return: 1 if the string is a palindrome, 0 otherwise.
- *
- * Example:
- * If s is "level", the function will return 1 (a palindrome).
- * If s is "hello", the function will return 0 (not a palindrome).
- * If s is an empty string, the function will return 1 (an empty string is a palindrome).
+ * Return: int
  */
+
 int is_palindrome(char *s)
 {
-    int len = strlen(s);
-    return check_palindrome(s, 0, len - 1);
-}
+	int start = 0;
+	int end = strlen(s) - 1;
 
-/**
- * check_palindrome - Recursive helper function to check palindrome.
- * @s: The string to check.
- * @start: The starting index.
- * @end: The ending index.
- *
- * Return: 1 if the string is a palindrome, 0 otherwise.
- */
-int check_palindrome(char *s, int start, int end)
-{
-    if (start >= end)
-        return 1;
+	while (start < end)
+	{
+	if (s[start] != s[end])
+		return (0);
 
-    if (s[start] != s[end])
-        return 0;
+		start++;
+		end--;
+	}
 
-    return check_palindrome(s, start + 1, end - 1);
+	return (1);
 }
