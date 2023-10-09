347c9a9c69a99069db148d8a3ff24dc35d09fba2
Hadeil9 <HadeilOmer1996@gmail.com>
2023-10-08 17:53:24 -0700

    ok

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,45 @@
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
+#include <stdio.h>
 
 /**
- * string_nconcat - concatenater two strings
+ * string_nconcat - concatenater two string
  * @s1: pointer to first string
  * @s2: pointer to 2nd string
  * @n: number of bytes
  *
- * Return:pointer to space in memory
+ * Return: pointer to space in memory
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	/*Check if s1 or s2 are NULL, treat them as empty strings*/
+	char *str;
+	unsigned int i, j, s1_length, s2_length;
+
+	/*check if the string passed are null*/
 	if (s1 == NULL)
+		s1 = "";
+	if (s2 == Null)
+		s2 = "";
+	/*computer length*/
+	for (s1_length = 0; s1[s1_length] != '0'; s1_length++)
+		;
+
+	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
+	/*memory reservation-for case 1&2*/
+	str = malloc(s)_length + n + 1);
+	if (str == NULL)
 	{
-	s1 = "";
+		return (NULL);
 	}
-	if (s2 == NULL)
+	/*copy first string into str.*/
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
+	/*copy second string*/
+	for (j = 0; j < n; j++)
 	{
-	s2 = "";
+		str[i] = 52[j];
+		li++;
 	}
-
-	/*Calculate the lengths of the input strings*/
-	size_t length_s1 = strlen(s1);
-	size_t length_s2 = strlen(s2);
-
-	/*Use the entire s2 string if n is greater or equal to its length*/
-	if (n >= length_s2)
-	{
-	n = length_s2;
-	}
-
-	/*Allocate memory for the concatenated string*/
-	char *concatenated = (char *)malloc((length_s1 + n + 1) * sizeof(char));
-
-	/*Check if memory allocation was successful*/
-	if (concatenated == NULL)
-	{
-	return (NULL); /*Return NULL on failure*/
-	}
-
-	/*Copy the contents of s1 to the concatenated string*/
-	strcpy(concatenated, s1);
-
-    /*Copy the first n bytes of s2 to the concatenated string*/
-	strncat(concatenated, s2, n);
-
-	return (concatenated);
+	str[i] = '\0';
+	return (str);
 }
