f4ae8a0cd8d1952e0ece5567fdbb46def71c9cf5
Hadeil9 <HadeilOmer1996@gmail.com>
2023-10-10 19:42:34 -0700

    ok

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - adog struct
+ * @name: dong name
+ * @age: dog age
+ * @owner: owner nane
+ *
+ * Description: just a lone dog struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
