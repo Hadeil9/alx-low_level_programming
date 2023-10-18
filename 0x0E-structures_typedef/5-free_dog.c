6210e73b2e99a2ec963aa9cca6791e1a884eda16
Hadeil9 <HadeilOmer1996@gmail.com>
2023-10-18 16:30:06 -0700

    ok

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,14 +4,18 @@
 /**
  * free_dog - frees memory allocated for a struct dog
  * @d: struct dog to free
+ *
+ * Return: void
  */
 
 void free_dog(dog_t *d)
 {
 	if (d)
 	{
-		free(d->name);
-		free(d->owner);
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
 		free(d);
 	}
 }
